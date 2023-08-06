eb309dec14135dfb702b97a231a256d3acb8f234
Yavin Torres <yavintorres@gmail.com>
2023-08-06 04:43:39 -0700

    Render Images and forEach card info

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log('connected')
+const renderImages = (dataArray) => {
+    console.log(dataArray)
+    dataArray.forEach(cardInfo => {
+        console.log(cardInfo)
+    })
+}
 
 fetch("https://isro.vercel.app/api/spacecrafts")
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => renderImages(data.spacecrafts))
