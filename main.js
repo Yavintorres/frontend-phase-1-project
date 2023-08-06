8789a990b36dd137107e2004997230adb94cdf8a
Yavin Torres <yavintorres@gmail.com>
2023-08-06 04:46:37 -0700

    const imgaes

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ console.log('connected')
 const renderImages = (dataArray) => {
     console.log(dataArray)
     dataArray.forEach(cardInfo => {
-        console.log(cardInfo)
+        console.log(cardInfo.name)
+        const img = document.createElement("img")
+        console.log(img)
     })
 }
 
